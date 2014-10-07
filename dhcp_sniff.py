877491accd790db7361611de9a6a57d65bcc7540
Pengfei Zhang <lalasjtu@gmail.com>
2014-10-07 17:09:06 +0800

    configure iface name for mac

--- dhcp_sniff.py
+++ dhcp_sniff.py
@@ -12,6 +12,9 @@ import sys
 conf.checkIPaddr = False
 
 # Set up the interface
+
+conf.iface="en1" #set this as the interface of the server
+
 fam,hw = get_if_raw_hwaddr(conf.iface)
 
 dhcp_discover = Ether(dst="ff:ff:ff:ff:ff:ff")/IP(src="0.0.0.0",dst="255.255.255.255")/UDP(sport=68,dport=67)/BOOTP(chaddr=hw)/DHCP(options=[("message-type","discover"),"end"])
